56a84fa2f82d46f00fd12746d0b9cc136c111b5a
Trinity Terry <trinitycterry@gmail.com>
2020-01-17 10:13:41 -0600

    called obj function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const pope = {
     name: "Francis",
-    age: 70,
+    age: 83,
     hasTallHat: true,
     bless: function(blessee){
         console.log(`I bless you, ${blessee}`);
     }
 };
 
-console.log(`The Pope is named ${pope.name}`);
+console.log(`The Pope is named ${pope.name}. He is ${pope.age} years old`);
+
+pope.bless("Bryan");
