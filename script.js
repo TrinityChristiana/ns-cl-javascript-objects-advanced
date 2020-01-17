1e96467e67b52369fb8c154b867f7ed18abca995
Trinity Terry <trinitycterry@gmail.com>
2020-01-17 10:32:24 -0600

    Added more birthdays

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const pope = {
     hasTallHat: true,
     bless: function(blessee){
         console.log(`I bless you, ${blessee}`);
+    },
+    celebrateBirthday: function() {
+        console.log(`Yay!`);
+        this.age++;
     }
 };
 
-console.log(`The Pope is named ${pope.name}. He is ${pope.age} years old`);
+const tellMeAboutThePope = () => {
+    console.log(`The Pope is named ${pope.name}. He is ${pope.age} years old`);
+};
 
+tellMeAboutThePope();
 pope.bless("Bryan");
+pope.celebrateBirthday();
+tellMeAboutThePope();
